c5bbcc4f2c9e3e694a0914f4c36599500d9f21d3
Mahmoud94ha <mahmoudhannani3@gmail.com>
2024-02-20 02:10:10 +0100

    React Redux Connectors and Providers

---
+++ react_redux_connectors_and_providers/task_5/dashboard/src/schema/notifications.js
@@ -0,0 +1,44 @@
+import * as notificationsData from "../../notifications.json";
+import { normalize, schema } from "normalizr";
+
+const user = new schema.Entity("users");
+
+const message = new schema.Entity(
+  "messages",
+  {},
+  {
+    idAttribute: "guid",
+  }
+);
+
+const notification = new schema.Entity("notifications", {
+  author: user,
+  context: message,
+});
+
+const normalizedData = normalize(notificationsData.default, [notification]);
+
+export { normalizedData };
+
+export function getAllNotificationsByUser(userId) {
+  const notifications = normalizedData.entities.notifications;
+  const messages = normalizedData.entities.messages;
+
+  const userNotifications = [];
+
+  for (const property in notifications) {
+    if (notifications[property].author === userId) {
+      userNotifications.push(messages[notifications[property].context]);
+    }
+  }
+
+  return userNotifications;
+}
+
+const notificationsNormalizer = (data) => {
+  const normalizedData = normalize(data, [notification]);
+
+  return normalizedData.entities;
+};
+
+export default notificationsNormalizer;
